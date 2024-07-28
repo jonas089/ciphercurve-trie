492fc39bb59c5abfaf197ad8dbd3e4c3990deaf9
jonas pauli <jonaspauli089@gmail.com>
2024-07-29 00:16:14 +0200

    init: merkle proofs

--- src/merkle.rs
+++ src/merkle.rs
@@ -1,6 +1,28 @@
 // Compute Merkle Proof for a Leaf at a given point in time (e.g. at a Snapshot)
-use crate::store::db::InMemoryDB;
+use crate::store::{db::InMemoryDB, types::Node};
 // obtain the merkle path for a leaf
 pub fn merkle_proof(db: &mut InMemoryDB, key: Vec<u8>) {
     assert_eq!(key.len(), 256);
+}
+
+/* Todo
+Construct merkle path for a node given the path
+
+Root
+-> left (0)
+-> right (1)
+
+Branch
+-> prefix[] or [...]
+if []: current_idx 0: left, current_idx 1: right
+if [...] current_idx [...] 0: left, current_idx [...] 1: right
+
+Leaf
+-> ok
+
+collect all siblings in a list and re-hash up to the root to verify.
+*/
+
+struct MerkleProof {
+    nodes: Vec<(bool, Node)>,
 }
